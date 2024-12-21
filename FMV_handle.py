fd24dcf7baa6f57ed7167336a70721f42c688c65
wilson <qwe789qwec@gmail.com>
2024-12-21 16:16:36 +0900

    start_scan

--- FMV_handle.py
+++ FMV_handle.py
@@ -10,90 +10,145 @@ from pathlib import Path
 
 reader = easyocr.Reader(['en'], gpu = True)
 
-class triple_town_handler:
-    def __init__(self, save_dir='gameplay'):
-        self.step = 0
-        self.save_dir = save_dir
-        self.game_number = self.get_next_game_number()
+class FMV_handler:
+    def __init__(self, item_dir = 'item_template', size = 9):
+        pyautogui.PAUSE = 0.1
+        self.item_dir = item_dir
         self.init_mouse_position()
-        # slot for game and recognize
-        self.slot_gap = 80
-        self.slot_size = 60
+        self.init_screen_position()
+        time.sleep(1)
+        matrix = self.scan_slot()
+        fullmap = matrix
+        for i in range(int(size/3)):
+            matrix = self.scan_slot()
+            fullmap = np.concatenate((fullmap, matrix), axis=1)
+            self.go_up()
 
-        self.score = 0
-        self.last_score = 0
+        print(fullmap)
+
+        # 9-17
 
     def init_mouse_position(self):
         # get window position
-        self.screen_x, self.screen_y = self.get_game_position()
-        self.screen_w, self.screen_h = 802, 639
+        self.gift_x, self.gift_y = self.get_game_position()
+        if self.gift_x is None:
+            time.sleep(1)
+            self.gift_x, self.gift_y = self.get_game_position()
+            if self.gift_x is None:
+                print("Failed to get window position.")
+                return
+        print(f"gift position: ({self.gift_x}, {self.gift_y})")
 
-        # mouse standby and play game init
-        self.mouse_x = self.screen_x + 533
-        self.mouse_y = self.screen_y + 108
-        self.game_x = self.screen_x + 70
-        self.game_y = self.screen_y + 160
+        # slot size
+        self.slot_w = 60
+        self.slot_h = 50
+        self.slot_gap = 77
+        self.slot_gap_y = 68
+        self.slot_angle = np.arctan(0.34/0.7)
 
-        # score region
-        self.score_x = 100
-        self.score_y = 40
-        self.score_w = 380
-        self.score_h = 50
+        # game area
+        self.screen_x = self.gift_x - 700
+        self.screen_y = self.gift_y - 660
+        self.screen_w = 1400
+        self.screen_h = 770
 
-        # end game and start game position
-        self.end_x = self.screen_x + 682
-        self.end_y = self.screen_y + 247
-        self.start_x = self.screen_x + 97
-        self.start_y = self.screen_y + 198
+        self.drag_x = self.gift_x + 700
+        self.drag_y = self.gift_y - 300
+
+        self.init_slot_x = self.gift_x + 10
+        self.init_slot_y = self.gift_y - 196
+        self.init_slot_x, self.init_slot_y = self.slot_calculator(self.init_slot_x, self.init_slot_y, -8, 0)
+
+        self.scan_init_x, self.scan_init_y = 708, 460
+        self.scan_init_x, self.scan_init_y = self.slot_calculator(self.scan_init_x, self.scan_init_y, -8, 0)
+
+    def init_screen_position(self):
+        pyautogui.moveTo(self.gift_x, self.gift_y)
+        pyautogui.scroll(-30, x=None, y=None)
+        pyautogui.moveTo(self.drag_x, self.drag_y)
+        pyautogui.drag(0, -150, duration=0.1, button='left')
+        time.sleep(1.5)
+        pyautogui.drag(0, -150, duration=0.1, button='left')
+        time.sleep(1.5)
+        # pyautogui.drag(0, 40, duration=1, button='left')
+    
+    def go_up(self):
+        pyautogui.moveTo(self.drag_x, self.drag_y)
+        time.sleep(0.3)
+        pyautogui.mouseDown(button='left')
+        pyautogui.move(0, 199, duration=1)
+        time.sleep(0.3)
+        pyautogui.mouseUp(button='left')
+        pyautogui.click()
+
+    def slot_calculator(self, x, y, dir_x, dir_y):
+        goal_x = x + self.slot_gap * dir_x * np.cos(self.slot_angle)
+        goal_y = y - self.slot_gap * dir_x * np.sin(self.slot_angle)
+        goal_y = goal_y - self.slot_gap_y * dir_y
+        return int(goal_x), int(goal_y)
+    
+    def slot_region(self, pos_x, pos_y):
+        slot_x = pos_x - (self.slot_w / 2)
+        slot_y = pos_y - self.slot_h
+        return int(slot_x), int(slot_y)
 
     def get_game_position(self):
         screenshot = pyautogui.screenshot()
         screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
-        template = cv2.imread('window_template.png', cv2.IMREAD_COLOR)
+        template = cv2.imread('buttons/get_gift.png', cv2.IMREAD_COLOR)
+        # size of the template image
+        h, w, _ = template.shape
         result = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
         # get window position
         if max_val > 0.8:  # matching threshold
-            print(f"window position: {max_loc}")
-            return max_loc[0], max_loc[1]
+            # print(f"window position: {max_loc}")
+            return int(max_loc[0] + h/2), int(max_loc[1] + w/2)
         else:
             print("no matching window found!")
-            return -1, -1
-
-    def get_next_game_number(self):
-        os.makedirs(self.save_dir, exist_ok=True)
-        existing_files = os.listdir(self.save_dir)
-
-        game_numbers = []
-        for filename in existing_files:
-            if filename.startswith("game_") and filename.endswith(".png"):
-                try:
-                    # get the game number from the filename
-                    game_number = int(filename.split("_")[1])
-                    game_numbers.append(game_number)
-                except ValueError:
-                    pass  # ignore invalid filenames
-
-        return max(game_numbers, default=0) + 1
+            return None, None
     
     def take_screenshot(self):
-        pyautogui.moveTo(self.mouse_x, self.mouse_y)
+        pyautogui.moveTo(self.drag_x, self.drag_y)
         pyautogui.click()
         screenshot = pyautogui.screenshot(region=(self.screen_x, self.screen_y, self.screen_w, self.screen_h))
         frame = np.array(screenshot)
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         self.latest_image = frame
 
-    def save_image(self, image, action=-1):
+    def scan_slot(self, is_scan=True):
+        slot_matrix = np.full((17, 3), -1)
+        self.take_screenshot()
+
+        if is_scan:
+            slot_x, slot_y = self.slot_calculator(self.scan_init_x, self.scan_init_y, 16, -1)
+            scan_x, scan_y = self.slot_region(slot_x, slot_y)
+            slot_img = self.latest_image[scan_y:scan_y + self.slot_h, scan_x:scan_x + self.slot_w]
+            index = self.find_matching_item(slot_img)
+            slot_matrix[16, 2] = index
+            time.sleep(0.1)
+        
+        for i in range(17*3-1):
+            row, col = divmod(i, 17)
+            slot_x, slot_y = self.slot_calculator(self.scan_init_x, self.scan_init_y, col, row)
+            scan_x, scan_y = self.slot_region(slot_x, slot_y)
+            slot_img = self.latest_image[scan_y:scan_y + self.slot_h, scan_x:scan_x + self.slot_w]
+            index = self.find_matching_item(slot_img)
+            slot_matrix[col, row] = index
+            time.sleep(0.1)
+
+        return slot_matrix
+
+
+    def save_image(self, image, dir):
         if image is None:
             print("No image to save.")
             return
-        gameinfo = "_".join(map(str, self.state.flatten()))
         pil_image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        self.step = self.step + 1
-        filename = f"game_{self.game_number}_{self.step}_{action}_info_{self.next_item}_{self.score}_{gameinfo}.png"
-        pil_image.save(os.path.join(self.save_dir, filename))
+        next_item_id = self.get_next_path_id(dir)
+        filename = f"{next_item_id}.png"
+        pil_image.save(os.path.join(dir, filename))
 
     def get_score(self, take_screenshot=True):
         score_str = None
@@ -123,41 +178,7 @@ class triple_town_handler:
                 return self.last_score
         self.score = score
         return self.score
-
-    def slot_region(self, pos_x, pos_y):
-        slot_x = 70 + self.slot_gap * pos_x - (self.slot_size / 2)
-        slot_y = 160 + self.slot_gap * pos_y - (self.slot_size / 2)
-        return int(slot_x), int(slot_y)
     
-    def get_game_area(self, take_screenshot=True):
-        max_retries = 3
-        slot_matrix = np.full((6, 6), -1)
-
-        for slot in range(36):
-            row, col = divmod(slot, 6)
-            slot_x, slot_y = self.slot_region(row, col)
-            retry_count = 0
-            
-            while retry_count < max_retries:
-                slot_img = self.latest_image[slot_y:slot_y + self.slot_size, slot_x:slot_x + self.slot_size]
-                index = self.find_matching_item(slot_img)
-                if index >= 21:
-                    retry_count += 1
-                    slot_matrix[col, row] = 21
-                    if take_screenshot and retry_count < max_retries:
-                        print(f"Failed to recognize slot {slot}, taking screenshot.")
-                        time.sleep(1.5)
-                        self.take_screenshot()
-                else:
-                    slot_matrix[col, row] = index
-                    break
-
-        next_item = self.latest_image[85:85 + 60, 508:508 + 60]
-        next_item_id = self.find_matching_item(next_item)
-        self.state = slot_matrix
-        self.next_item = next_item_id
-        return slot_matrix, next_item_id
-
     def find_matching_item(self, item_image):
         max_match_value = 0.6
         matching_item_id = None
@@ -179,11 +200,11 @@ class triple_town_handler:
                         max_match_value = score
                         matching_item_id = item_folder
                     if score > 0.8 and matching_item_id is not None:
-                        if not template_file.startswith("0_"):
-                            new_name = f"0_{template_file}"
-                            new_path = os.path.join(item_folder_path, new_name)
-                            os.rename(template_image_path, new_path)
-                            print(f"Renamed: {template_image_path} -> {new_path}")
+                        # if not template_file.startswith("0_"):
+                        #     new_name = f"0_{template_file}"
+                        #     new_path = os.path.join(item_folder_path, new_name)
+                        #     os.rename(template_image_path, new_path)
+                        #     print(f"Renamed: {template_image_path} -> {new_path}")
                         break
 
         # if no matching item found, create a new folder
@@ -246,15 +267,6 @@ class triple_town_handler:
         pyautogui.click(self.start_x, self.start_y)
         time.sleep(5)
 
-test = False
+test = True
 if test:
-    gamesc = triple_town_handler()
-    gamesc.take_screenshot()
-    gamesc.latest_image = cv2.imread('save/game_1_0.png')
-    gamesc.click_slot(0)
-    gamesc.take_screenshot()
-    gamesc.save_image(gamesc.latest_image, 12)
-    state , next_item = gamesc.get_game_area()
-    print("state:\n", state)
-    print("next_item:", next_item)
-    print(gamesc.get_score())
+    game = FMV_handler()
