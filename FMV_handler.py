8cce5818d1253dabd0795c92999ba9ece484f98b
qwe789qwec <qwe789qwec@gmail.com>
2024-12-23 12:26:26 +0900

    new compare method

--- FMV_handler.py
+++ FMV_handler.py
@@ -1,126 +1,74 @@
 import pyautogui
 import time
+import shutil
 import os
 import cv2
 from skimage.metrics import structural_similarity as compare_ssim
 import numpy as np
 from PIL import Image
-import easyocr
 from pathlib import Path
-from collections import defaultdict
+from collections import namedtuple
 
-reader = easyocr.Reader(['en'], gpu = True)
+position = namedtuple('position', ['x', 'y'])
+region = namedtuple('region', ['x', 'y', 'w', 'h'])
+size = namedtuple('size', ['w', 'h'])
+temp_dir = "temp_images"
 
 class FMV_handler:
     def __init__(self, item_dir = 'item_template', scan_size = 9):
         pyautogui.PAUSE = 0.1
+        os.makedirs(temp_dir, exist_ok=True)
+        print(f"Temporary directory created: {temp_dir}")
         self.item_dir = item_dir
+        self.scan_size = scan_size
         self.init_mouse_position()
+        self.init_parameters()
         self.init_screen_position()
         # 9-17
 
     def init_mouse_position(self):
         # get window position
-        self.gift_x, self.gift_y = self.get_game_position()
-        if self.gift_x is None:
+        self.gift = self.get_item_position()
+        if self.gift.x is None:
             time.sleep(1)
-            self.gift_x, self.gift_y = self.get_game_position()
-            if self.gift_x is None:
+            self.gift = self.get_item_position()
+            if self.gift.x is None:
                 print("Failed to get window position.")
                 return
-        print(f"gift position: ({self.gift_x}, {self.gift_y})")
+        print(f"gift position: ({self.gift.x}, {self.gift.y})")
 
+        # game area
+        self.game_area = region(self.gift.x - 700, self.gift.y - 660, 1400, 770)
+        self.drag = position(self.gift.x + 700, self.gift.y - 300)
+    
+    def init_parameters(self):
         # slot size
-        self.slot_w = 60
-        self.slot_h = 50
+        self.item_size = size(60, 50)
+        self.slot_size = size(60, 50)
         self.slot_gap = 77
         self.slot_gap_y = 68
         self.slot_angle = np.arctan(0.34/0.7)
 
-        # game area
-        self.screen_x = self.gift_x - 700
-        self.screen_y = self.gift_y - 660
-        self.screen_w = 1400
-        self.screen_h = 770
+        self.scan_go_up = 199
+        self.play_go_up = 66
+        self.play_go_down = 330
 
-        self.drag_x = self.gift_x + 700
-        self.drag_y = self.gift_y - 300
+        self.scan_relative_position = position(0, 0)
+        self.play_relative_position = position(0, 0)
 
-        self.init_slot_x = self.gift_x + 10
-        self.init_slot_y = self.gift_y - 196
-        self.init_slot_x, self.init_slot_y = self.slot_calculator(self.init_slot_x, self.init_slot_y, -8, 0)
-
-        self.scan_init_x, self.scan_init_y = 708, 460
-        self.scan_init_x, self.scan_init_y = self.slot_calculator(self.scan_init_x, self.scan_init_y, -8, 0)
-
-        self.game_x = self.gift_x + 10
-        self.game_y = self.gift_y - 95
-        self.game_x, self.game_y = self.slot_calculator(self.game_x, self.game_y, -8, 0)
-
-    def init_screen_position(self):
-        pyautogui.moveTo(self.gift_x, self.gift_y)
-        pyautogui.scroll(-30, x=None, y=None)
-        pyautogui.moveTo(self.drag_x, self.drag_y)
-        pyautogui.drag(0, -150, duration=0.1, button='left')
-        time.sleep(1.5)
-        pyautogui.drag(0, -150, duration=0.1, button='left')
-        time.sleep(1.5)
-        # pyautogui.drag(0, 40, duration=1, button='left')
-
-    def init_play_position(self):
-        pyautogui.moveTo(self.gift_x, self.gift_y)
-        pyautogui.scroll(-30, x=None, y=None)
-        pyautogui.moveTo(self.drag_x, self.drag_y)
-        pyautogui.drag(0, -150, duration=0.1, button='left')
-        time.sleep(1.5)
-        pyautogui.drag(0, -150, duration=0.1, button='left')
-        time.sleep(1.5)
-        pyautogui.drag(0, 40, duration=0.6, button='left')
-        time.sleep(1.5)
-    
-    def scan_go_up(self):
-        pyautogui.moveTo(self.drag_x, self.drag_y)
-        time.sleep(0.3)
-        pyautogui.mouseDown(button='left')
-        pyautogui.move(0, 199, duration=1)
-        time.sleep(0.3)
-        pyautogui.mouseUp(button='left')
+    def take_screenshot(self, region=None):
+        pyautogui.moveTo(self.drag.x, self.drag.y)
         pyautogui.click()
-    
-    def play_go_up(self):
-        pyautogui.moveTo(self.drag_x, self.drag_y)
-        time.sleep(0.3)
-        pyautogui.mouseDown(button='left')
-        # pyautogui.move(0, 330, duration=1)
-        pyautogui.move(0, 66, duration=1)
-        time.sleep(0.3)
-        pyautogui.mouseUp(button='left')
-        pyautogui.click()
-
-    def play_go_down(self):
-        pyautogui.moveTo(self.drag_x, self.drag_y)
-        time.sleep(0.3)
-        pyautogui.mouseDown(button='left')
-        pyautogui.move(0, -330, duration=1)
-        time.sleep(0.3)
-        pyautogui.mouseUp(button='left')
-        pyautogui.click()
-
-    def slot_calculator(self, x, y, dir_x, dir_y):
-        goal_x = x + self.slot_gap * dir_x * np.cos(self.slot_angle)
-        goal_y = y - self.slot_gap * dir_x * np.sin(self.slot_angle)
-        goal_y = goal_y - self.slot_gap_y * dir_y
-        return int(goal_x), int(goal_y)
-    
-    def slot_region(self, pos_x, pos_y):
-        slot_x = pos_x - (self.slot_w / 2)
-        slot_y = pos_y - self.slot_h
-        return int(slot_x), int(slot_y)
-
-    def get_game_position(self):
         screenshot = pyautogui.screenshot()
-        screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
-        template = cv2.imread('buttons/get_gift.png', cv2.IMREAD_COLOR)
+        frame = np.array(screenshot)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        if region is not None:
+            frame = frame[region.y:region.y + region.h, region.x:region.x + region.w]
+        return frame
+    
+    def get_item_position(self, region=None,item_name='buttons/get_gift.png'):
+        screenshot = self.take_screenshot(region)
+        template = cv2.imread(item_name, cv2.IMREAD_COLOR)
         # size of the template image
         h, w, _ = template.shape
         result = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)
@@ -129,72 +77,88 @@ class FMV_handler:
         # get window position
         if max_val > 0.8:  # matching threshold
             # print(f"window position: {max_loc}")
-            return int(max_loc[0] + h/2), int(max_loc[1] + w/2)
+            return position(int(max_loc[0] + h/2), int(max_loc[1] + w/2))
         else:
             print("no matching window found!")
             return None, None
-    
-    def take_screenshot(self):
-        pyautogui.moveTo(self.drag_x, self.drag_y)
-        pyautogui.click()
-        screenshot = pyautogui.screenshot(region=(self.screen_x, self.screen_y, self.screen_w, self.screen_h))
-        frame = np.array(screenshot)
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        self.latest_image = frame
-
-    def adjust_position(self, x, y):
-        slot_x, slot_y = self.slot_calculator(x, y, 8, 0)
-        scan_x, scan_y = self.slot_region(slot_x, slot_y)
-        slot_img = self.latest_image[scan_y:scan_y + self.slot_h, scan_x:scan_x + self.slot_w]
-        index = self.find_matching_item(slot_img)
-        print("get", index)
-        game_image_gray = cv2.cvtColor(self.latest_image, cv2.COLOR_BGR2GRAY)
-        template_image_path = os.path.join("item_template", str(index), "0.png")
-        template_image = cv2.imread(template_image_path, cv2.IMREAD_GRAYSCALE)
-        method = cv2.TM_CCOEFF_NORMED
-        result = cv2.matchTemplate(game_image_gray, template_image, method)
-        _, _, _, max_loc = cv2.minMaxLoc(result)
-        print("location", max_loc)
-        loc_x, loc_y = max_loc
-        if (loc_x + loc_y) - (scan_x + scan_y) > 150:
-            print("too far")
-            return x, y
-        adj_x, adj_y = self.slot_calculator(int(loc_x + (self.slot_w/2)), int(loc_y + (self.slot_h/2)), -8, 0)
-
-        return adj_x, adj_y
-
-    def scan_slot(self, is_scan=True):
-        slot_matrix = np.full((17, 3), -1)
-        self.take_screenshot()
-        # self.scan_init_x, self.scan_init_y = self.adjust_position(self.scan_init_x, self.scan_init_y)
-
-        if is_scan:
-            slot_x, slot_y = self.slot_calculator(self.scan_init_x, self.scan_init_y, 16, -1)
-            scan_x, scan_y = self.slot_region(slot_x, slot_y)
-            slot_img = self.latest_image[scan_y:scan_y + self.slot_h, scan_x:scan_x + self.slot_w]
-            index = self.find_matching_item(slot_img)
-            if index > 160:
-                slot_matrix[16, 2] = -1
-            else:
-                slot_matrix[16, 2] = index
-            time.sleep(0.1)
-        else:
-            slot_matrix[16, 2] = -1
-            time.sleep(0.1)
         
+    def init_screen_position(self):
+        pyautogui.moveTo(self.gift.x, self.gift.y)
+        pyautogui.scroll(-30, x=None, y=None)
+        pyautogui.moveTo(self.drag.x, self.drag.y)
+        pyautogui.drag(0, -150, duration=0.1, button='left')
+        time.sleep(1.5)
+        pyautogui.drag(0, -150, duration=0.1, button='left')
+        time.sleep(1.5)
+        # pyautogui.drag(0, 40, duration=1, button='left')
+
+    def init_play_position(self):
+        self.init_screen_position()
+        pyautogui.drag(0, 40, duration=0.6, button='left')
+        time.sleep(1.5)
+
+    def screen_slider(self, distance):
+        pyautogui.moveTo(self.drag.x, self.drag.y)
+        time.sleep(0.3)
+        pyautogui.mouseDown(button='left')
+        pyautogui.move(0, distance, duration=1)
+        time.sleep(0.3)
+        pyautogui.mouseUp(button='left')
+        pyautogui.click()
+
+    def slot_calculator(self, pos, dir_x, dir_y):
+        goal_x = int(pos.x + self.slot_gap * dir_x * np.cos(self.slot_angle))
+        goal_y = pos.y - self.slot_gap * dir_x * np.sin(self.slot_angle)
+        goal_y = int(goal_y - self.slot_gap_y * dir_y)
+        return position(goal_x, goal_y)
+    
+    def item_region(self, pos, region_size):
+        slot_x = pos.x - (region_size.w / 2)
+        slot_y = pos.y - (region_size.h / 2)
+        region = region(slot_x, slot_y, region_size.w, region_size.h)
+        return region
+
+    def scan_slot(self):
+        # slot_matrix = np.full((17, 3), -1)
+        station_pos = self.get_item_position(region=self.game_area, item_name='buttons/station.png')
+        init_scan = position(station_pos.x + self.scan_relative_position.x, station_pos.y + self.scan_relative_position.y)
+        game_image = self.take_screenshot(region=self.game_area)
+
+        scan_pos = self.slot_calculator(init_scan, 16, -1)
+        slot_region = self.item_region(scan_pos, self.slot_size)
+        slot_img = game_image[slot_region.y:slot_region.y + self.slot_size.h, slot_region.x:slot_region.x + self.slot_size.w]
+        self.save_image(slot_img, temp_dir)
+        item_region = self.item_region(scan_pos, self.item_size)
+        item_img = game_image[item_region.y:item_region.y + self.item_size.h, item_region.x:item_region.x + self.item_size.w]
+        self.save_image(item_img, temp_dir)
+
         for i in range(17*3-1):
             row, col = divmod(i, 17)
-            slot_x, slot_y = self.slot_calculator(self.scan_init_x, self.scan_init_y, col, row)
-            scan_x, scan_y = self.slot_region(slot_x, slot_y)
-            slot_img = self.latest_image[scan_y:scan_y + self.slot_h, scan_x:scan_x + self.slot_w]
-            index = self.find_matching_item(slot_img)
-            if index > 150:
-                slot_matrix[col, row] = -1
-            else:
-                slot_matrix[col, row] = index
-            time.sleep(0.1)
+            scan_pos = self.slot_calculator(init_scan, col, row)
+            slot_region = self.item_region(scan_pos, self.slot_size)
+            slot_img = game_image[slot_region.y:slot_region.y + self.slot_size.h, slot_region.x:slot_region.x + self.slot_size.w]
+            self.save_image(slot_img, temp_dir)
+            item_region = self.item_region(scan_pos, self.item_size)
+            item_img = game_image[item_region.y:item_region.y + self.item_size.h, item_region.x:item_region.x + self.item_size.w]
+            self.save_image(item_img, temp_dir)
+    
+    def compare_slot_image(self):
+        image_id = [
+            int(os.path.splitext(image_file)[0])
+            for image_file in os.listdir(temp_dir)
+            if os.path.splitext(image_file)[0].isdigit()
+        ]
 
-        return slot_matrix
+        file_number = max(image_id, default=0)
+
+        for i in range(file_number - 2):
+            i = i + 2
+            image_path = os.path.join(temp_dir, f"{i}.png")
+            image = cv2.imread(image_path)
+            item_id = self.find_matching_item(image)
+            image_id.append(item_id)
+
+        print(image_id)
 
     def swap_item(self, from_x, from_y, to_x, to_y):
         pyautogui.moveTo(from_x, from_y)
@@ -210,35 +174,6 @@ class FMV_handler:
         next_item_id = self.get_next_path_id(dir)
         filename = f"{next_item_id}.png"
         pil_image.save(os.path.join(dir, filename))
-
-    def get_score(self, take_screenshot=True):
-        score_str = None
-        score = 0
-        check_time = 0
-
-        while score is None and check_time < 2:
-            score_region = self.latest_image[self.score_y:self.score_y + self.score_h, self.score_x:self.score_x + self.score_w]
-            gray = cv2.cvtColor(score_region, cv2.COLOR_BGR2GRAY)
-            _, thresh_image = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
-            score_str = reader.readtext(thresh_image)
-            check_time += 1
-            if score_str is None and check_time < 2 and take_screenshot:
-                time.sleep(1)
-                self.take_screenshot()
-        
-        if score_str == [] or score_str is None:
-            score = 0
-        else:
-            try:
-                if score_str[0][1].count(',') > 0:
-                    score = int(score_str[0][1].replace(',', ''))
-                else:
-                    score = int(score_str[0][1])
-                self.last_score = score
-            except ValueError:
-                return self.last_score
-        self.score = score
-        return self.score
     
     def find_matching_item(self, item_image):
         max_match_value = 0.6
@@ -301,6 +236,10 @@ class FMV_handler:
             return 0
 
         return max(existing_ids) + 1
+    
+    def __exit__(self):
+        shutil.rmtree(temp_dir)
+        print(f"Temporary directory deleted: {temp_dir}")
 
 test = False
 if test:
